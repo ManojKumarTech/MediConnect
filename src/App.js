a274026d8c68f0278c6c90b90a4964a62916a21d
NITHESH-E <nith22064.it@rmkec.ac.in>
2024-01-09 21:12:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1 id="logo">Oppurtunity<br></br> Hub</h1>
+    <div className="main">
+      <div className="sub-main">  
+      
+        <div>
+          <h1 id="login">Login</h1>
+          <h5 id="pass">Email</h5> 
+          <input id="input"type="text" placeholder="Enter Email id"></input>
+          <h5 id="pass">Password</h5>
+          <input id="input"type="password" placeholder="Enter Password"></input><br></br>
+          <h6 id="fp">Forget Password ?</h6>  
+          <button id="submit_B"type="submit">submit</button>          
+          </div>
+      </div>
     </div>
+    </>
   );
 }
 
