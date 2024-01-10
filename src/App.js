9c93b31104aeda5e42a1a548eb6f96b490b872a6
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-01-10 09:59:21 +0530

    small changes in color

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
   return (
     
     <>
-    <h1 id="logo">Oppurtunity<br></br> Hub</h1>
+    <h1 id="logo">MediConnect<br></br></h1>
     <div className="main">
       <div className="sub-main">  
       
         <div>
           <h1 id="login">Login</h1>
-          <h5 id="pass">Email</h5> 
+          <h5 id="pass">Email</h5>
           <input id="input"type="text" placeholder="Enter Email id"></input>
           <h5 id="pass">Password</h5>
           <input id="input"type="password" placeholder="Enter Password"></input><br></br>
-          <h6 id="fp">Forget Password ?</h6>  
-          <button id="submit_B"type="submit">submit</button>          
+          <h6 id="fp" >Forget Password ?</h6>
+          <button id="submit_B"type="submit">submit</button>
           </div>
       </div>
     </div>
