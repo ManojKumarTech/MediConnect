8330a3e81751c53ebd0b8e017e39d50f17920722
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-01-11 22:03:55 +0530

    Creating navbar

--- src/App.js
+++ src/App.js
@@ -12,8 +12,53 @@ function App() {
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
     crossOrigin="anonymous"
   />
-    
-    <h1 id="logo">MediConnect<br></br></h1>
+
+  {/* NavBar */}
+  <link
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    rel="stylesheet"
+  />
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#" style={{color:"green",fontSize:"30px"}}>
+      MediConnect
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#" style={{color:"green",fontSize:"18px"}}>
+            Home <span className="sr-only">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#" style={{color:"green",fontSize:"18px"}}>
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#" style={{color:"green",fontSize:"18px"}}>
+            Services
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#" style={{color:"green",fontSize:"18px"}} >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  {/* NavBar */}
     <div className="main">
       <div className="sub-main">
       
