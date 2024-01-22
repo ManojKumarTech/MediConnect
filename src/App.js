05a41d3a4dc19acb292b2cc9dfd348cc2c77d9a5
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-01-22 14:52:23 +0530

    imported Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import react from 'react';
 import './App.css';
 import Login from './Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-          <>
-          <Login/>
-          </>
-      );
+    <BrowserRouter>
+    <Routes>
+
+    <Route
+        path="/login"
+        element={
+            <Login />
+        }
+      />
+
+    </Routes>
+</BrowserRouter>
+);
 }
 
 export default App;
