ccbb70518adf879716413caf86cd3809fcbc5da3
ManojkumarS9543 <mano22056.it@rmkec.ac.in>
2024-01-25 10:53:01 +0530

    Created a Home page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import react from 'react';
 import './App.css';
 import Login from './pages/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './pages/Footer';
+import Thome from './pages/Thome';
 
 function App() {
   return (
+    
     <BrowserRouter>
     <Routes>
+
+      <Route
+          path="/home"
+          element={
+            <Thome/>
+          }
+      />
 
     <Route
         path="/login"
@@ -15,9 +25,16 @@ function App() {
             <Login />
         }
       />
+    
+    <Route
+        path="/footer"
+        element={
+          <Footer/>
+        }
+      />
 
     </Routes>
-</BrowserRouter>
+    </BrowserRouter>
 );
 }
 
---
+++ src/pages/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer()
+{
+const year = new Date();
+
+    return(
+        <div className='main'>
+        <footer>Copyright &copy; {year.getFullYear()} </footer>
+        </div>
+        
+    )
+}
+export default Footer
