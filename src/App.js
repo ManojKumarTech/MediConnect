4558d4eadab30b11b404ccd1d67ba70c2e440257
gkeerthivasan <gkeerthivasan2005@gmail.com>
2025-07-17 18:38:53 +0530

    New change

--- src/App.js
+++ src/App.js
@@ -1,92 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Registration from './pages/Registration';
-import Thome from './pages/Thome';
-import Bookingpage from './pages/Bookingpage';
-import OrderPlacedPage from './pages/orderPlaced';
-import HomePage from './pages/Home';
-import MedicineProp from './pages/MedicineProps';
-import FeedBack from './pages/FeedBack';
-import FeedbackSubmited from './pages/FeedbackSubmited';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Registration from "./pages/Registration";
+import Thome from "./pages/Thome";
+import Bookingpage from "./pages/Bookingpage";
+import OrderPlacedPage from "./pages/orderPlaced";
+import HomePage from "./pages/Home";
+import MedicineProp from "./pages/MedicineProps";
+import FeedBack from "./pages/FeedBack";
+import FeedbackSubmited from "./pages/FeedbackSubmited";
 //import Portfolio from './pages/Portfolio';
-
 
 function App() {
   return (
-
-
     <BrowserRouter>
       <Routes>
+        <Route path="/hospitals" element={<Thome />} />
 
-        <Route
-          path="/hospitals"
-          element={
-            <Thome />
-          }
-        />
+        <Route path="" element={<Registration />} />
 
-        <Route
-          path=""
-          element={
-            <Registration />
-          }
-        />
+        <Route path="/order" element={<Bookingpage />} />
 
-        <Route
-          path='/order'
-          element={
-            <Bookingpage />
-          }
-        />
+        <Route path="/medicines/order" element={<Bookingpage />} />
 
-        <Route
-          path='/medicines/order'
-          element={
-            <Bookingpage />
-          }
-        />
+        <Route path="/medicines" element={<MedicineProp />} />
 
-        <Route
-          path="/medicines"
-          element={
-            <MedicineProp />
-          }
-        />
+        <Route path="/feedbacksubmited" element={<FeedbackSubmited />} />
 
-        <Route
-          path='/feedbacksubmited'
-          element={
-            <FeedbackSubmited/>
-          }
-        />
+        <Route path="/home" element={<HomePage />} />
 
+        <Route path="/orderplaced" element={<OrderPlacedPage />} />
 
-        <Route
-          path='/home'
-          element={
-            <HomePage />
-          }
-        />
-
-        <Route
-          path='/orderplaced'
-          element={
-            <OrderPlacedPage />
-          }
-        />
-
-        <Route
-          path='/feedback'
-          element={
-            <FeedBack />
-          }
-        />
-
-      
-
+        <Route path="/feedback" element={<FeedBack />} />
       </Routes>
     </BrowserRouter>
-
   );
 }
 
