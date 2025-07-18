8afcf5177ae21f7210a3c972b773a21801a184b6
gkeerthivasan <gkeerthivasan2005@gmail.com>
2025-07-18 08:55:50 +0530

    firebase change

--- src/firebase/setup.js
+++ src/firebase/setup.js
@@ -1,19 +1,17 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBDosUxkQkizHJSPi_i8fo5Ye0NQILVAPU",
-    authDomain: "fir-auth-d2273.firebaseapp.com",
-    projectId: "fir-auth-d2273",
-    storageBucket: "fir-auth-d2273.appspot.com",
-    messagingSenderId: "590383500686",
-    appId: "1:590383500686:web:ec275f32fb9da643ee28df",
-    measurementId: "G-LN9TQDVQQS"
-  };
-
+  apiKey: "AIzaSyBDosUxkQkizHJSPi_i8fo5Ye0NQILVAPU",
+  authDomain: "fir-auth-d2273.firebaseapp.com",
+  projectId: "fir-auth-d2273",
+  storageBucket: "fir-auth-d2273.appspot.com",
+  messagingSenderId: "590383500686",
+  appId: "1:590383500686:web:ec275f32fb9da643ee28df",
+  measurementId: "G-LN9TQDVQQS",
+};
 
 const app = initializeApp(firebaseConfig);
 export const database = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
